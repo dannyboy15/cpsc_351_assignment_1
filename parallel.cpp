7fa46ea1f3b9fb25edfee32783e904a49c39d34b
Daniel Bravo <bravoappdev@gmail.com>
2017-07-22 13:43:49 -0700

    Added variables for wget path and urls file path. Implemented parent process wait based serial or parallel.

--- parallel.cpp
+++ parallel.cpp
@@ -9,68 +9,71 @@ using namespace std;
 
 /**
  * Forks children and lets them perform their tasks
- * @param urls - the urls to download
+ * @param urls - the URLs to download
+ * @parama path - the path to the wget command
  */
-void create_children(vector<string>& urls)
-{	
+void create_children(vector<string>& urls, string path) {	
+	
+	/* Counter for the number of processes */
+	int numForks = 0;
+
 	/* The process id */
 	pid_t pid;
 	
 	/* Go through all the URLs */
 	for(vector<string>::iterator urlIt = urls.begin();
-			urlIt != urls.end(); ++urlIt)
-	{
+			urlIt != urls.end(); ++urlIt) {
+
 		/* Create a child */
+		numForks++;
 		pid = fork();
 
-
 		/* Make sure the fork was a success */
-		if(pid < 0)
-		{
+		if(pid < 0) {
 			perror("fork:");
 			exit(1);
 		}
 		/* The child code */
-		if(pid == 0)
-		{	
-			
-	fclose(stdout);
-	fclose(stderr);
-			
+		else if(pid == 0) {
+			fclose(stdout);
+			fclose(stderr);
+
 			/* Deploy wget */
-			if(execlp("/usr/bin/wget", "child", urlIt->c_str(), NULL) < 0)
-			{
+			if(execlp(path.c_str(), "wget", urlIt->c_str(), NULL) < 0) {
 				perror("execlp");
 				exit(1);
 			}
 				
 		}
-	}	
+	}
+
+	/* Wait until no children are left */
+	while(numForks > 0) {
+        wait(NULL);
+        numForks--;
+    }
 }
 
 /**
  * Read the URLs from the file
  * @param urls - the URLs to download
+ * @param file - the filename of the URLs file
  */
-
-void readUrls(vector<string>& urls)
-{
+void readUrls(vector<string>& urls, string file) {
 	/* Open the file */
-	ifstream urlFile("urls.txt");		
+	ifstream urlFile(file);		
 	
 	/* The URL buffer */
 	string urlBuffer;
 	
 	/* Make sure the file was opened */
-	if(!urlFile.is_open())
-	{
+	if(!urlFile.is_open()) {
 		fprintf(stderr, "Failed to open the file");
 		exit(1);
 	}
 	
 	/* Read the entire file */
-	while(!urlFile.eof())
-	{
+	while(!urlFile.eof()) {
 		/* Read the buffer */
 		urlFile >> urlBuffer;
 		
@@ -83,20 +86,26 @@ void readUrls(vector<string>& urls)
 	urlFile.close();
 }
 
-int main()
-{
-	
+int main() {
+	// File with URLs to download
+	string urlsFile = "urls.txt";
+
+	// For macOS
+	string cmdPath = "/usr/local/bin/wget";
+	// For Linux
+	// string cmdPath = "/usr/bin/wget"
 		
 	/* Read the URL files */
 	vector<string> urls;
 		
 	/* Read the URLs */
-	readUrls(urls);
+	readUrls(urls, urlsFile);
 	
 	/* Create child processes */
-	create_children(urls);
+	create_children(urls, cmdPath);
 	
-	wait(NULL);
+	// wait(NULL);
+	printf("Complete\n");
 		
 	return 0;	
 	
--- serial.cpp
+++ serial.cpp
@@ -9,41 +9,42 @@ using namespace std;
 
 /**
  * Forks children and lets them perform their tasks
- * @param urls - the urls to download
+ * @param urls - the URLs to download
+ * @parama path - the path to the wget command
  */
-void create_children(vector<string>& urls)
-{	
+void create_children(vector<string>& urls, string path) {	
+	
 	/* The process id */
 	pid_t pid;
 	
 	/* Go through all the URLs */
 	for(vector<string>::iterator urlIt = urls.begin();
-			urlIt != urls.end(); ++urlIt)
-	{
+			urlIt != urls.end(); ++urlIt) {
+		
 		/* Create a child */
 		pid = fork();
 
-
 		/* Make sure the fork was a success */
-		if(pid < 0)
-		{
+		if(pid < 0) {
 			perror("fork:");
 			exit(1);
 		}
 		/* The child code */
-		if(pid == 0)
-		{	
-			
-	fclose(stdout);
-	fclose(stderr);
-			
+		else if(pid == 0) {
+			fclose(stdout);
+			fclose(stderr);
+
 			/* Deploy wget */
-			if(execlp("/usr/bin/wget", "child", urlIt->c_str(), NULL) < 0)
-			{
+			if(execlp(path.c_str(), "wget", urlIt->c_str(), NULL) < 0) {
 				perror("execlp");
 				exit(1);
 			}
 				
+		}
+		else { /* parent process */
+			/* parent will wait for the child to complete */
+	        wait(NULL);
+	        printf("Child Complete\n");
 		}
 	}	
 }
@@ -51,26 +52,23 @@ void create_children(vector<string>& urls)
 /**
  * Read the URLs from the file
  * @param urls - the URLs to download
+ * @param file - the filename of the URLs file
  */
-
-void readUrls(vector<string>& urls)
-{
+void readUrls(vector<string>& urls, string file) {
 	/* Open the file */
-	ifstream urlFile("urls.txt");		
+	ifstream urlFile(file);		
 	
 	/* The URL buffer */
 	string urlBuffer;
 	
 	/* Make sure the file was opened */
-	if(!urlFile.is_open())
-	{
+	if(!urlFile.is_open()) {
 		fprintf(stderr, "Failed to open the file");
 		exit(1);
 	}
 	
 	/* Read the entire file */
-	while(!urlFile.eof())
-	{
+	while(!urlFile.eof()) {
 		/* Read the buffer */
 		urlFile >> urlBuffer;
 		
@@ -83,20 +81,26 @@ void readUrls(vector<string>& urls)
 	urlFile.close();
 }
 
-int main()
-{
-	
+int main() {
+	// File with URLs to download
+	string urlsFile = "urls.txt";
+
+	// For macOS
+	string cmdPath = "/usr/local/bin/wget";
+	// For Linux
+	// string cmdPath = "/usr/bin/wget"
 		
 	/* Read the URL files */
 	vector<string> urls;
 		
 	/* Read the URLs */
-	readUrls(urls);
+	readUrls(urls, urlsFile);
 	
 	/* Create child processes */
-	create_children(urls);
+	create_children(urls, cmdPath);
 	
-	wait(NULL);
+	// wait(NULL);
+	printf("Complete\n");
 		
 	return 0;	
 	
